a3db8a5fbf45d4211e5a2787e2062b77363140c9
emincin.macos <emincin.life@gmail.com>
2026-01-28 12:28:06 +0800

    Update main.c

--- text-editor/main.c
+++ text-editor/main.c
@@ -1,12 +1,19 @@
 #include <termios.h>
 #include <unistd.h>
 
+#include <stdio.h>
+
 void enable_raw_mode(void) {
+    int ret = 0;
+    struct termios io = {0};
+    ret = tcgetattr(STDIN_FILENO, &io);
 }
 
 void disable_raw_mode(void) {
 }
 
 int main(int argc, char** argv) {
+    enable_raw_mode();
+    disable_raw_mode();
     return 0;
 }
