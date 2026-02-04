52f2de4c04f102f348dc849c39763a5903d6dd83
emincin.macos <emincin.life@gmail.com>
2026-02-04 17:49:54 +0800

    Update main.c

--- text-editor/main.c
+++ text-editor/main.c
@@ -10,6 +10,18 @@
 
 #define SN(s) (s), (sizeof(s) - 1)
 
+typedef int8_t i8;
+typedef uint8_t u8;
+
+typedef int16_t i16;
+typedef uint16_t u16;
+
+typedef int32_t i32;
+typedef uint32_t u32;
+
+typedef int64_t i64;
+typedef uint64_t u64;
+
 typedef intptr_t isize;
 typedef uintptr_t usize;
 
